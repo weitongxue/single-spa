8cb95421951f914aa8be9cfe48a4644141a871a2
caiwei <caiwei@baibu.la>
2020-09-18 16:41:50 +0800

    vue.config.js抽离优化

--- packages/app1/src/router.js
+++ packages/app1/src/router.js
@@ -1,14 +1,7 @@
 import Router from 'vue-router'
-import Home from './components/HelloWorld.vue'
 
 export default new Router({
   // mode: 'history',
   // base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/app1',
-      name: 'home',
-      component: Home
-    }
-  ]
+  routes: []
 })
